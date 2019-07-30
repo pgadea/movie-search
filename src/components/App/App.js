38b9ff8954a5c9945cac2f23ddef12fdea69921a
Pierre <pgadea@users.noreply.github.com>
2019-07-29 23:33:09 -0500

    added subfolder path to BrowserRouter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ import NotFound from '../elements/NotFound/NotFound';
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename='/react-movie-search/'>
             <React.Fragment>
                 <Header />
                 <Switch>
